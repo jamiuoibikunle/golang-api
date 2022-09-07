b74f90d236e354ed2d99f5d921de52c04c59de7c
jamiu ibikunle <jamiuoibikunle@gmail.com>
2022-09-07 16:13:18 +0100

    First push

--- models/States..go
+++
@@ -1,9 +0,0 @@
-package models
-
-import "go.mongodb.org/mongo-driver/bson/primitive"
-
-type State struct {
-	Id      primitive.ObjectID `json: "id, omitempty"`
-	Name    string             `json: "state, omitempty" validate: "required"`
-	Capital string             `json: "capital, omitempty" validate: "required"`
-}
